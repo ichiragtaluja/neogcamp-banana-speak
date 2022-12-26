391f26e38f3e3eb3b189d706210dbea4bb1c0a3e
ichiragtaluja <Ichiragtaluja@gmail.com>
2022-12-26 19:55:06 +0530

    first draft

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ var textInput = document.querySelector("#text-input");
 
 var outputDiv = document.querySelector("#output");
 
-outputDiv.innerText = "ggg"
+//varServerURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+varServerURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text) {
+    return (varServerURL + "?text==" + text)
+}
+
+function errorHandler(error) {
+    console.log("Error Identified", error)
+
+}
+
 
 function clickHandler() {
-    outputDiv.innerText = "hjhgjgj" + textInput.value;
-};
+
+    var inputText = textInput.value
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
+
+}
 
 
 btnTranslate.addEventListener("click", clickHandler)
