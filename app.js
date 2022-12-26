43e5ed8c9abfe3258478e2c7087e6079f56c9ee4
ichiragtaluja <Ichiragtaluja@gmail.com>
2022-12-26 16:39:52 +0530

    done input output

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var textInput = document.querySelector("#text-input");
 
-var outputDiv = document.querySelector("#output"); 
+var outputDiv = document.querySelector("#output");
 
-outputDiv.innerText ="ggg"
+outputDiv.innerText = "ggg"
 
 function clickHandler() {
-    outputDiv.innerText ="hjhgjgj" + textInput.value;
+    outputDiv.innerText = "hjhgjgj" + textInput.value;
 };
 
 
 btnTranslate.addEventListener("click", clickHandler)
-
-
